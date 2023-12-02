7f96ca9ee4868ad748e1ffb8ff7a3fe7ea090a99
Wong Jun Kit <five5over@gmail.com>
2023-12-02 16:02:35 +0800

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
     const [word, setWord] = useState('');
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        console.log(images);
+    }, [images]);
 
     const handleSearchSubmit = (e) => {
         e.preventDefault();
@@ -16,13 +23,20 @@ const App = () => {
         )
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
+                setImages([{ ...data, title: word }, ...images]);
             })
             .catch((err) => {
                 console.log(err);
             });
 
         setWord('');
+    };
+
+    const handleDeleteImage = (id) => {
+        const filterCondition = (image) => {
+            return image.id !== id;
+        };
+        setImages(images.filter(filterCondition));
     };
 
     return (
@@ -33,6 +47,20 @@ const App = () => {
                 setWord={setWord}
                 handleSubmit={handleSearchSubmit}
             />
+            <Container className="mt-4">
+                <Row xs={1} md={2} lg={3}>
+                    {images.map((image, i) => {
+                        return (
+                            <Col key={i} className="pb-3">
+                                <ImageCard
+                                    image={image}
+                                    deleteImage={handleDeleteImage}
+                                />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
         </div>
     );
 };
